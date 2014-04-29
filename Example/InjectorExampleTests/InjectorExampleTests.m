41308ee7d50e5ea60e766f986d640d09e71901d6
Eric-Paul Lecluse <e@epologee.com>
2014-04-29 16:23:06 +0200

    Add new empty iPhone project

---
+++ Example/InjectorExampleTests/InjectorExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  InjectorExampleTests.m
+//  InjectorExampleTests
+//
+//  Created by Eric-Paul Lecluse on 29-04-14.
+//  Copyright (c) 2014 epologee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface InjectorExampleTests : XCTestCase
+
+@end
+
+@implementation InjectorExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
